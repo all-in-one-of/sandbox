a74fb42c1d476389b04491a2a2e38d55fcf2f800
Claudio <claudiohick@gmail.com>
2018-06-20 17:12:18 -0300

    python file test

---
+++ git_tests/39641_python1_python.py
@@ -0,0 +1,12 @@
+import hou
+import os
+hou.node()
+print "----------------LISTA DE OUTPUT NODES ---------------------------"
+pathHip = os.path.split(hou.hipFile.name())
+pathfull = str(pathHip[0] + "/log.txt")
+f = open(pathfull,"w") 
+for node in hou.node("/out/").allSubChildren():
+        result = node.path()
+        print result
+        f.write(result + "\n") 
+f.close()
